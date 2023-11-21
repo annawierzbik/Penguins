b6be082529aa5b65f10418c7d050160a6c896621
akopys <01190696@pw.edu.pl>
2023-11-21 13:14:35 +0100

    Upload New File

---
+++ move.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "move.h"
+
+void move_penguin(){
+	//choose penguin
+	//if penguin cannot move go back to: choose penguin
+	// (possibly) display/highlight all the possible moves for the penguin
+	//choose coordinates
+	//check if coordinates are valid
+	//(if they were anomg the ones displayed earlier)
+	//if not go back to: choose penguin
+	//else change coordinates
+	//collect fish
+	//change value of the current floe to -1
+}
