f61bdc2f53d8b842cbe59d8c8c6b3a61de174186
akopys <01190696@pw.edu.pl>
2023-11-21 13:12:59 +0100

    Upload New File

---
+++ board.h
@@ -0,0 +1,8 @@
+#include "structures.h"
+
+void initialize_board(int, int, int [][N]);
+void create_board(int, int, int [][N]);
+int ones_on_board(int, int, int [][N]);
+int is_board_good(int, int, int, int, int [][N]);
+void display_board(int, int, int [][N]);
+
