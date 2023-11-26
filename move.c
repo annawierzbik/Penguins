f3d2fab955355f24bd4827cc5f3cab6f99699e30
jterroba <01190827@pw.edu.pl>
2023-11-26 19:45:00 +0100

    Update move.c

--- move.c
+++ move.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
 #include "move.h"
 
+void makeMove(struct Board *board, int *ROW , int *COLUMN) {
+    char direction;
+    printf("Ingrese la dirección del movimiento (u/d/l/r): ");
+    scanf(" %c", &direction);
+
+    int nuevaFila = *ROW;
+    int nuevaColumna = *COLUMN;
+
+    // Actualizar la posición de la ficha según la dirección ingresada
+    switch (direction) {
+        case 'u':
+            if (nuevaFila > 0) {
+                nuevaFila--;
+            }
+            break;
+        case 'd':
+            if (nuevaFila < 99) {
+                nuevaFila++;
+            }
+            break;
+        case 'l':
+            if (nuevaColumna > 0) {
+                nuevaColumna--;
+            }
+            break;
+        case 'r':
+            if (nuevaColumna < 99) {
+                nuevaColumna++;
+            }
+            break;
+        default:
+            printf("Dirección no válida. Intente de nuevo.\n");
+            return; // Salir de la función en caso de dirección no válida
+    }
+
+    // Verificar si la nueva posición está ocupada
+    if (    board->cells[nuevaFila][nuevaColumna] == ' ') {
+        // La nueva posición está libre, realizar el movimiento
+        board->cells[*ROW][*COLUMN] = ' ';
+        board->cells[nuevaFila][nuevaColumna] = 'X'; // Suponiendo que la ficha es 'X'
+        *ROW = nuevaFila;
+        *COLUMN = nuevaColumna;
+    } else {
+        printf("La posición está ocupada. Intente de nuevo.\n");
+    }
+}
+// this function is only for the movement of the penguin, it should be used after we have checked that the penguin can move. in case there is no posible movement it will tell you. to indicate where do you wanna place the penguins i have thought of indicating it using u(UP), d (down), l(LEFT), r(RIGHT).
+
 void move_penguin(){
 	//choose penguin
 	//if penguin cannot move go back to: choose penguin
