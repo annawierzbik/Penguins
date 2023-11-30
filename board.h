7bd882b715c8a43659f4ad71810f10cc126dd4a6
jterroba <01190827@pw.edu.pl>
2023-11-30 12:40:23 +0100

    Update board.h

--- board.h
+++ board.h
@@ -1,11 +1,26 @@
 #include "structures.h"
 
 #ifndef _BOARDF_
-
+//first two parameter are n and m, that are the dimensions of the game
+//int [][N] is introducing the board itself
 void initialize_board(int, int, int [][N]);
+
+//first two parameter are n and m, that are the dimensions of the game
+//int [][N] is introducing the board itself
 void create_board(int, int, int [][N]);
+
+//first two parameter are n and m, that are the dimensions of the game
+//int [][N] is introducing the board itself
 int ones_on_board(int, int, int [][N]);
+
+//first two parameter are n and m, that are the dimensions of the game
+//the third command should be the number of players 
+// the fourth parameter should be 
+//int [][N] is introducing the board itself
 int is_board_good(int, int, int, int, int [][N]);
+
+//first two parameter are n and m, that are the dimensions of the game
+//int [][N] is introducing the board itself
 void display_board(int, int, int [][N]);
 
 #endif //_BOARDF_
