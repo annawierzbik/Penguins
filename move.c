a2ddffff6a6bf3372c4bd397718749ce45d7b614
jterroba <01190827@pw.edu.pl>
2023-12-13 15:52:57 +0100

    Update move.c

--- move.c
+++ move.c
@@ -4,26 +4,93 @@
 
 int is_available(int n, int m, int x, int y, int board[][N]){
 
+     bool size_ok;
     //is on board nxm?
-    //is not equal 0?
+    if (x<n || y<m){
+        printf("\nThese coordinates are not in your board. Please try again.\n");
+        size_ok=false;
+        return 0;
+    }
+    else{
+            size_ok=true;
+    }
     //is not other penguin?
-
+    if (size_ok==true && board [x][y]==players_letter[]){
+        printf("\nThere is a penguin in this cell. Please try again.\n");
+        return 0;
+    }
+    //is not equal 0?
+    else if (size_ok==true && board [x][y]==0){
+        printf("\nThere is an ice flow in this cell. Please try again.\n");
+        return 0;
+    }
+    else{
     return 1;//1 if available, 0 if not
+    }
 }
 
 int can_penguin_move(int n, int m, struct coordinates penguin, int board[][N]){
+    //first we save the coordinates of the penguin.
+    int x=input.x;
+    int y=input.y;
+    //we create for variables for each of the possible four movements
+    int up, down, left, right;
+    //we check the movements in the x axes
+    up=is_available(int n, int m, int x+1, int y, int board[][N]);
+    down=is_available(int n, int m, int x-1, int y, int board[][N]);
+    //we check the movement in the y axes
+    left=is_available(int n, int m, int x, int y+1, int board[][N]);
+    right=is_available(int n, int m, int x, int y-1, int board[][N]);
 
-    return 1;//1 if it can move, 0 if it can't
+    //we check if any of the values is 1
+    if (up==1 || down==1 || left==1 || right==1){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+//1 if it can move, 0 if it can't
 }
 
 int can_player_move(int n, int m, int pen, struct player* player, int board[][N]){
+ bool playerok_move;// variable to check if there is a possibility of moving one of the penguins
+    //we look for each of the penguins of a player if they can move
 
-    return 1;//1 if any can, 0 if not
+    for (int i=0; i<pen, i++){//it is okay called to each of the penguins
+        int penguin;
+        penguin=can_penguin_move(n, m, penguin[i], board [][N] )//¿is okay used the struct?
+        //if the penguin can move then we change the previous parameter
+        if (penguin==1){
+            playerok_move==true;
+        }
+    }
+//we check now if we have found a possible move or if there are no possible movements
+    if (playerok_move==true){
+        return 1;
+    }
+    else{
+        return 0;
+    }//1 if any can, 0 if not
+    
 }
 
 int can_any_move(int n, int m, int pla, int pen, struct player players[], int board[][N]){
 
-    return 1;//1 if any can, 0 if not
+     bool any_move; // parameter to check if anyone can move a penguin
+        for (int i=0; i<pla, i++){//it is okay called to each of the penguins
+            int player;
+                player=can_player_move(n, m,pen, player[i], board [][N] )//¿is okay used the struct?
+        //if the penguin can move then we change the previous parameter
+            if (player==1){
+                any_move==true;
+            }
+        }
+        if (any_move==true){
+            return 1;
+        }
+        else{
+            return 0;
+        }//1 if any can, 0 if not
 }
 
 int which_penguin(int x, int y, int pen, struct player* player){
