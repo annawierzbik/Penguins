41074818cfafd7f61a50f387935c95aeb2c06f02
akopys <01190696@pw.edu.pl>
2023-12-26 13:56:35 +0100

    As we will be creating an autonomous mode, I renamed main as main_interactive and added the file main_autonomous. Both can use all the functions in the other files, you compile only the main for the mode you intend to use.

---
+++ main_autonomous.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "structures.h"
+#include "setup.h"
+#include "board.h"
+#include "penguin.h"
+#include "game.h"
+
+int main(int argc, char *argv[]){
+
+//This is the file for autonomous mode. 
+//BOTH THE INPUT AND THE OUTPUT, AS WELL AS WHAT IS RETURNED IN THIS FILE ARE 
+//SPECIFIED IN THE PENGUINS_V2 FILE IN TEAMS.
+//It must be compatible with the specifications - pay attention to them.
+
+//For this tutorial we need:
+//command line parameters
+//reading the state of the game from file ACCORDING TO SPECIFICATIONS
+//writing the state of the game to file ACCORDING TO SPECIFICATIONS
+
+
+
+
+
+    return 0;
+}
